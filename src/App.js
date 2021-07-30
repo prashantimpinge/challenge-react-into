e754e02d6dba1ef36ada856e764fc1f4b0be1bb3
prashant luthra <harsukhbir.impinge@gmail.com>
2021-07-30 18:23:45 +0530

    commited

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <p className="header">
+          I have profound experience in analysis, design and development of various websites, web applications and desktop applications. I have been working extensively in PHP, MySQL,React,AJAX, Java Scripting, Html, CSS, Jquery, NodeJS. In addition, I have worked on customizing and writing modules and plugins for third part softwares like vBulletin and working on Metronic Admin Theme.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
